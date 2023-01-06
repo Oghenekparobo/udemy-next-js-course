7fa38e1ee908d5fbd4e6c52ae503f5516c3d7d5a
STEPHEN ONOKWAKPOR <classicsteph11@gmail.com>
2023-01-06 11:41:16 +0100

    updated img tage to Image

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,9 +1,11 @@
 import classes from "./meetupdetails.module.css";
+import Image from "next/image";
+
 
 const MeetupDetail = (props) => {
   return (
     <section className={classes.detail} >
-      <img src={props.image} alt={props.title} />
+      <Image src={props.image} alt={props.title} />
       <h1>{props.title}</h1>
       <h3>{props.description}</h3>
       <h6>{props.address}</h6>
